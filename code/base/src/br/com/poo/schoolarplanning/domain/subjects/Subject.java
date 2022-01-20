246450900399bc30265c2785ca76522b8c80cc6c
Victor Martins <martinsvictor6781@gmail.com>
2022-01-20 08:50:10 -0300

    base da classe subject

--- code/base/src/br/com/poo/schoolarplanning/domain/subjects/Subject.java
+++ code/base/src/br/com/poo/schoolarplanning/domain/subjects/Subject.java
@@ -4,4 +4,15 @@ public class Subject {
     private String name;
     private String description;
     private String teacherName;
+
+    public Subject(String name, String teacherName, String description){
+        this.name = name;
+        this.teacherName = teacherName;
+        this.description = description;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Name: %s\nTeacher: %s\nDescription: %s", this.name, this.teacherName, this.description);
+    }
 }
